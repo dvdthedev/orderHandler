1df85afbfdd787af5eda3c3807c99d2b139c94af
dvd20 <dvd2.0@live.com>
2025-06-03 15:07:28 -0300

    add 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        String path = "E://Projetos//Java//orderHandler//file.csv";
 
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+
+            String line = br.readLine();
+
+            while (line != null){
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
