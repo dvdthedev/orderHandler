f9f298268169fc6ade2d2156942edcee9f6d70c1
dvd20 <dvd2.0@live.com>
2025-06-03 16:20:42 -0300

    add 1

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,33 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String path = "E://Projetos//Java//orderHandler//file.csv";
+        String strPath = "E:\\Projetos\\Java\\orderHandler";
+        File diretorio = new File(strPath + "\\out\\summary.csv");
 
-        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+        if(!diretorio.exists()) {
+            boolean success = new File(strPath + "\\out\\summary.csv").mkdir();
+            System.out.println("Directory created successfully: " + success);
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(strPath + "//file.csv"))){
 
             String line = br.readLine();
 
+            ArrayList<Product> products = new ArrayList<Product>();
+
             while (line != null){
-                System.out.println(line);
+                String[] keys = line.split(";");
+                System.out.println(Arrays.stream(keys).toList());
+                Product product = new Product(keys[0], Double.valueOf(keys[1]), Integer.valueOf(keys[2]));
+                System.out.println(product.getName() + " " + product.getTotal());
                 line = br.readLine();
             }
         } catch (IOException e){
--- src/Product.java
+++ src/Product.java
@@ -1,3 +1,18 @@
-public record Product (String name, Double value, int quantity) {
+public class Product {
+    private String name;
+    private Double total;
 
+    public Product(String name, Double value, int quantity) {
+        this.name = name;
+        this.total = value * quantity;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
 }
