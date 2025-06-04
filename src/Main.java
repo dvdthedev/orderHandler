7745bc079951a7960c91b6d30d1fd5656da171cf
dvd20 <dvd2.0@live.com>
2025-06-04 11:48:22 -0300

    partial solution

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,30 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         String strPath = "E:\\Projetos\\Java\\orderHandler";
-        File diretorio = new File(strPath + "\\out\\summary.csv");
+        File diretorio = new File(strPath + "\\out");
 
         if(!diretorio.exists()) {
-            boolean success = new File(strPath + "\\out\\summary.csv").mkdir();
+            boolean success = new File(strPath + "\\out").mkdir();
             System.out.println("Directory created successfully: " + success);
         }
 
-        try (BufferedReader br = new BufferedReader(new FileReader(strPath + "//file.csv"))){
+        try (BufferedReader br = new BufferedReader(new FileReader(strPath + "\\file.csv"))){
+
 
             String line = br.readLine();
 
-            ArrayList<Product> products = new ArrayList<Product>();
-
+            BufferedWriter bfw = new BufferedWriter(new FileWriter(strPath + "\\out\\summary.csv"));
             while (line != null){
                 String[] keys = line.split(";");
-                System.out.println(Arrays.stream(keys).toList());
+
                 Product product = new Product(keys[0], Double.valueOf(keys[1]), Integer.valueOf(keys[2]));
                 System.out.println(product.getName() + " " + product.getTotal());
                 line = br.readLine();
+                bfw.write(product.getName() + ";" + product.getTotal() + "\n");
             }
+            bfw.close();
         } catch (IOException e){
             System.out.println("Error: " + e.getMessage());
         }
